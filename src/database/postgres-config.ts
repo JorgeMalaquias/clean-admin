0983b0464b3defecbb7d1802de7545cda284b8a8
jorge <jorgemalaquia@gmail.com>
2024-03-12 19:58:54 -0300

    chore: postgres sets and add @types/pg

---
+++ src/database/postgres-config.ts
@@ -0,0 +1,20 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const port = Number(process.env.POST);
+const database = process.env.DATABASE;
+
+
+const connection = new Pool({
+  user,
+  password,
+  host,
+  port,
+  database
+});
+
+export default connection.query;
